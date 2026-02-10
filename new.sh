b611663bdd399d7496df2860416c0fe38ae6947e
FA <finneganadair@gmail.com>
2026-02-10 10:39:27 -0500

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+import JPanel
+public class Main {
+  public static void main(String[] args) {
+    japenl = new jpanel("blue");
+    console.log("Hello Wolrd!")
+    if(false = true):
+	console.log("true is true");
+    mov %RSP, 80
+    SELECT * FROM helloworld;
+    malloc(*"hello world");
+    sudo tcpdump > new.sh&&
+    printf("\a");
+    @ @@@@@�@@@@��@@@@��-@�-@..@�@@$$��@�@@  @ @  S�t
+d��@�@@P�td��@�@44Q
+�tdR�td��-@�GNUc�   q���b��3�� ���H�H��,H��t��H���5�,�%�,
+@�%�,h������%�,h�������1�I��^H��H���PTE1�1�H��@�#,�f.����f.�
+���1@H=�1@t�H��t       ��1@��f��ff.�@��1@H��1@H��H��?H��H�H��t�H��t
+��1@���ff.�@���=1-uUH���z����-]Ð�ff.�@���UH��H��`�E�'�E��E�E�
+�E��E��Z�E�����u��������E��E�ЉE��E���~*�M�Hc�Hi�gfffH�� ��������)
+����)��ʉU��E��}�      ~�H�E�H�E��AH�E��E����H�E��E�H�H��H@0@��E�H�H��H@0@����E��E�
+E
+
+
+����u-�E�H�H��H@0@��U�Hc�H�
+�H��@0@���������U�����H�E�1ЉE��}�u�E��M�Hc�Hi�VUUUH�� H����)���
+)��ʅ�u$�E�H�H��H@0@��E�H�H��H@0@����E�H�H��H@0@�
+�E�H�H��H�0@�1ЈE�H�E�=�~�E�H�H�U�H
+��E߈��E�H�H�U�H��E߈�E��E�;E�������E�H��D�H�E�H�ƿ�@���������UH��H��0��@������H�E�H�ƿ�@������H�E�H�ƿ�
+@����������H�H��/lib64/ld-linux-x86
+   				for i in {1............5}; make
+					dir; done
+  }
+}
